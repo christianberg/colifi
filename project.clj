e83a37cb15e91dc478f3580ada3fdb0eaa77c5b7
Christian Berg <berg.christian@gmail.com>
2011-03-28 14:49:04 +0200

    Added lein-swank dependency.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject colifi "0.1.0-SNAPSHOT"
   :description "Command Line Finance"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[swank-clojure "1.3.0"]])
